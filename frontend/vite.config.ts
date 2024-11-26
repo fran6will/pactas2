69f5b113a0db2dec5d0bd55a3c7236f60f48cc67
Francis-William Rhéaume <francisrheaume@gmail.com>
2024-11-26 08:57:30 -0500

    packagejson_vite.config up

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'components': path.resolve(__dirname, './src/components'),
+      'pages': path.resolve(__dirname, './src/pages'),
+      'context': path.resolve(__dirname, './src/context'),
+      'styles': path.resolve(__dirname, './src/styles'),
+    }
+  },
   build: {
     outDir: 'dist',
-    sourcemap: true,
-  },
-  css: {
-    modules: {
-      localsConvention: 'camelCase',
-    },
-  },
-})
+    sourcemap: true
+  }
+});
